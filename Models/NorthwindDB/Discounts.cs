793905a395d868e9ab9d7a2eabf290c2050b5d84
BIT student <bitstudent@bitnet.wctc.edu>
2019-04-29 12:07:15 -0500

    Add project files.

---
+++ Models/NorthwindDB/Discounts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Northwind.Models
+{
+    public partial class Discounts
+    {
+        public int DiscountId { get; set; }
+        public int? Code { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public int? ProductId { get; set; }
+        public decimal? DiscountPercent { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public virtual Products Product { get; set; }
+    }
+}
